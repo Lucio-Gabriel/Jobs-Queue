fb59413ed5a06124e5d46a137bd8bc6eb237e36a
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-08-29 10:30:02 -0300

    feat: create Controller and Request

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\UserRegistered;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function __invoke(RegisterRequest $request)
+    {
+        $input = $request->validated();
+
+        $user = User::query()->create($input);
+
+        // Dispara o evento
+        UserRegistered::dispatch($user);
+
+        return new UserResource($user);
+    }
 }
